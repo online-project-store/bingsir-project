f2500e0bb87cd864db0577c17f2b47a02159be11
Andy <29988076@qq.com>
2019-09-02 23:04:43 +0800

    react-redux use

--- web/bingsir-react/src/pages/home.jsx
+++ web/bingsir-react/src/pages/home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import http from '@/config/http';
 import api from '@/config/api';
 import { connect } from 'react-redux'
+import { increment, decrement, reset  } from "@/store/actions";
+console.log(increment);
+
 class Home extends React.Component {
     render() {
         return (
             <div>
-                {this.props.loading}
+                {this.props.count}
             </div>
         )
     }
@@ -18,23 +21,25 @@ class Home extends React.Component {
         })
     }
     componentDidMount(){
+        // console.log();
+        this.props.decrement()
         this.getData();
     }
 }
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
-        increase: (...args) => dispatch(actions.increase(...args)),
-        decrease: (...args) => dispatch(actions.decrease(...args))
+        // increase: (...args) => dispatch(actions.increase(...args)),
+        decrement: () => dispatch(decrement()),
+        increment: () => dispatch(increment()),
+        // decrease: (...args) => dispatch(actions.decrease(...args))
     }
 }
 
 
 function mapStateToProps(state) {
-    console.log(state);
-    
     return {
-        loading: state.counter.count,
+        count: state.counter.count,
     }
-    // 这里的state是react-redux store中的state，前面我们已经写过相关的代码，return { loading: false }
+    // 这里的state是react-redux store中的state，
 }
 export default connect(mapStateToProps, mapDispatchToProps )(Home);
--- web/bingsir-react/src/store/reducers/counter_reducer.js
+++ web/bingsir-react/src/store/reducers/counter_reducer.js
@@ -2,10 +2,10 @@ import obj from "../initState/index";
 const counterReducer = (state = obj, action) => {
     
     switch (action.type) {
-        case 'COUNT_ADD':
+        case 'DEC':
             return {
                 // ...state,
-                count: state.count + 1
+                count: state.count - 1
             }
             case 'COUNT_REDUCE':
                 return {
